a8a0e225e3465a534be13f28bee0ffc5c98811a9
yishikawa-jss <yuya_ishikawa@jrass.jp>
2022-04-26 23:13:52 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yuya/ishikawa/greetingapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package jp.techacademy.yuya.ishikawa.greetingapp
+
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button1) {
+            showTimePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+                this,
+                TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                    Log.d("UI_PARTS", "$hour:$minute")
+
+                    if (hour >= 2 && hour < 10) {
+                        textView.text = "おはよう"
+                    } else if (hour >= 10 && hour < 18) {
+                        textView.text = "こんにちは"
+                    } else {
+                        textView.text = "こんばんは"
+                    }
+
+                },
+                13, 0, true)
+        timePickerDialog.show()
+
+
+    }
+
+}
